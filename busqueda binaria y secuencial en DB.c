f699ea71de8ad3fab83a4db52a018c1b740a5ba6
RichieRod <116921287+RichieRod@users.noreply.github.com>
2023-05-31 10:01:30 -0600

    Update busqueda binaria y secuencial en DB.c

--- busqueda binaria y secuencial en DB.c
+++ busqueda binaria y secuencial en DB.c
@@ -9,7 +9,8 @@ int id_int[34595];
 char ch;
 char search[20];
 int contador = 0;
-
+int pos[1000];
+int pointer = 0;
 
 void buscar();  //Busqueda Secuencia
 void binaria(); //Busqueda Binaria
@@ -21,8 +22,12 @@ int main() {
     int opc;
 
     fp = fopen("AGUASCALIENTES.txt", "r+"); //modo lectura
-    leer_archivo();  
-    ordenar_archivo(); 
+    leer_archivo();  // Primero hay que obtener la informacion de interes del archivo
+    ordenar_archivo(); //vamos a ordenar el archivo para poder realizar una busqueda
+    // Binaria, no recomiendo que esta organizacion se haga dentro de la funcion que hace la busqueda
+    // porque cada que se haga una busqueda binaria va a organizar los datos y esto tomaria
+    //mucho tiempo, si lo hacemos al inicio de todo, se podra realizar la busqueda binaria
+    //mas rapido y todas las veces que sean necesarias
     do {
         system("cls");
         printf("\t****Base de Datos de Aguascalientes****\n\n");
@@ -46,7 +51,7 @@ int main() {
 
     } while (opc != 3);
 
-    fclose(fp);  //Cerrar el archvo 
+    fclose(fp);  // Es importante cerrar el archvo despues de usarlo
 
     system("pause");
     return 0;
@@ -56,9 +61,9 @@ void leer_archivo() {
     int i = 0, j = 0;
     do
     {
-        ch = fscanf(fp, "%s", ID[i]); //Obtener todos los string de 20 datos
-                                       
-        if (j == 20) {                  
+        ch = fscanf(fp, "%s", ID[i]); //Con esta funcion se van obteniendo todos los string
+                                       //del documento separados por espacio
+        if (j == 20) {                  // Hay 20 datos de cada persona, por eso el 20, no vamos a usarlos todos
             j = 1;
             i++;
         }
@@ -72,17 +77,19 @@ void leer_archivo() {
 
 }
 
-void ordenar_archivo() { 
+void ordenar_archivo() { // Para la busqueda binaria es necesario tener el archivo ordenado, lo ordenamos con el
+                        //metodo de la burbuja y esta de menor a mayor, esto solo se realiza una vez y queda listo
+                        //para usarse las veces necesarias
     int aux;
     int i,j;
     unsigned t1, t2;
     t1 = clock();
     for (i = 0; i < 34595; i++) {
-        id_int[i] = (int) strtol(ID[i], NULL, 10);  //Cast de string a entero 
-                                                    
+        id_int[i] = (int) strtol(ID[i], NULL, 10);  //del archivo txt obtuvimos string, hay que hacer un cast a entero 
+                                                    //para poder ordenar los numeros
     }
     
-    for (i = 0; i < 34595; i++) {       //Metodo de la burbuja organizados 
+    for (i = 0; i < 34595; i++) {       //Metodo de la burbuja organizados de menor a mayor
         for (j = 0; j < 34595; j++) {
             if (id_int[j] > id_int[j + 1]) {
                 aux = id_int[j + 1];
@@ -100,7 +107,7 @@ void ordenar_archivo() {
 
 }
 
-void buscar() {  //Busqueda secuencial entre strings
+void buscar() {  //Busqueda secuencial, en este caso si se realiza la comparacion entre strings
     unsigned t1, t2;
     
     system("cls");
@@ -111,7 +118,7 @@ void buscar() {  //Busqueda secuencial entre strings
     int i;
     t1 = clock();
     for (i = 0; i < 34595; i++) {
-        contador++;         //Iteraciones necesarias
+        contador++;         //Para indicar cuantas veces ha ejecutado el for, cuantas iteraciones fueron necesarias
         if (strcmp(ID[i], search) == 0) {
             no_found = 0;
             break;
@@ -136,7 +143,7 @@ void buscar() {  //Busqueda secuencial entre strings
 
 void binaria() {  // Busqueda Binaria
     int lim_inf, lim_sup, middle, count_1, busqueda2, i;
-    lim_sup = 34595;  //Cantidad de datos
+    lim_sup = 345950;  // Porque esta es la cantidad de datos, no confundir con el limite de los valores de la lista
     lim_inf = 0;
     count_1 = 0;
     int no_found = 1;
@@ -145,8 +152,8 @@ void binaria() {  // Busqueda Binaria
     scanf("%i", &busqueda2);
     t1 = clock();
     while (lim_inf <= lim_sup) {
-        count_1++;  //Iteraciones requeridas
-        middle = (lim_inf + lim_sup) / 2;  //Centro
+        count_1++;  // Este contador para saber cuantas iteraciones fueron requeridas
+        middle = (lim_inf + lim_sup) / 2;  // Posicion central
 
 
         if (busqueda2 == id_int[middle]) {
@@ -167,7 +174,10 @@ void binaria() {  // Busqueda Binaria
         }
 
 
+
     }
+
+    // Si no se encontro nada
 
     if (no_found == 1) {
         printf("ID no encontrado\n");
